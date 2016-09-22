b92c827c5303cd9ccb0e07e4156faa568daed29c
Paul Moff <moff.dev@gmail.com>
2016-09-22 15:49:20 +0700

    cleaned up code

--- module/module.ts
+++ module/module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule }            from '@angular/common';
 import { LoadersCssComponent }    from './loaders-css.component';
 
